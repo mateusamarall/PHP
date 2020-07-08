7223ee546aa3470e9fb30bf2a5bbb7a8dbfe320e
mateusamarall <mateus.amaral018@gmail.com>
2020-07-08 10:55:16 -0300

    constantes e arrays constantes no php

---
+++ aula15/exemplo.php
@@ -0,0 +1,6 @@
+<?php
+//constante
+define('SERVIDOR',"127.0.0.1");
+
+echo SERVIDOR;
+?>
---
+++ aula15/exemplo2.php
@@ -0,0 +1,10 @@
+<?php
+define('BANDO_DE_DADOS',[
+    '127.0.0.1',
+    'root',
+    'senha123',
+    'test'
+]);
+
+print_r(BANDO_DE_DADOS);
+?>
