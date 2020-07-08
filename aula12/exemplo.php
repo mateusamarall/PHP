a3b773500459b3f5d98ccd5ce0c79e46e1f1b750
mateusamarall <mateus.amaral018@gmail.com>
2020-07-08 09:55:28 -0300

    estrutura while em php

---
+++ aula12/exemplo.php
@@ -0,0 +1,17 @@
+<?php
+
+$condicao = true;
+
+while($condicao){
+    $numero = rand(1,10);
+
+    if($numero ===3){
+        echo " BINGOO ";
+
+        $condicao =false;
+    }
+    echo $numero . " ";
+}
+
+
+?>
