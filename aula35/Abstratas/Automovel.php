831ec61baa2738e836be610862e4a701a747bb10
mateusamarall <mateus.amaral018@gmail.com>
2020-07-14 09:43:41 -0300

    Incluindo classes com Autoload

---
+++ aula35/Abstratas/Automovel.php
@@ -0,0 +1,22 @@
+<?php
+
+interface Veiculo{
+    public function acelerar($velocidade);
+    public function frenar($velocidade);
+    public function trocarMarcha($marcha);
+
+}
+abstract class Automovel implements Veiculo{
+
+    public function acelerar($velocidade){
+        echo "o veículo acelerou até ". $velocidade . " km/h";
+    }
+    public function frenar($velocidade){
+        echo "O veículo frenou até " . $velocidade . " km/h";
+    }
+
+    public function trocarMarcha($marcha){
+        echo "O veículo engatou a marcha " .$marcha;
+    }
+}
+?>
