1d79cd56c56a8ad78866d7a5ea4acdc1a9717d73
mateusamarall <mateus.amaral018@gmail.com>
2020-07-15 12:34:42 -0300

    função para atualizar usuário

--- DAO/class/Users.php
+++ DAO/class/Users.php
@@ -150,6 +150,20 @@ class Users{
         $this->setDessenha($password);
     }
 
+    public function update($login, $password){
+
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID",
+        array(
+            ':LOGIN'=>$this->getDeslogin(),
+            ':PASSWORD'=>$this->getDessenha(),
+            ':ID'=>$this->getIdusuario()
+        ));
+    }
+
     //retornar um usuário pelo ID
     public function __toString(){
         return json_encode(array(
