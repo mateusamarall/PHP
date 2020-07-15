722960a8024ea55e3c22fc795fbd2b9b08011773
mateusamarall <mateus.amaral018@gmail.com>
2020-07-15 11:37:31 -0300

    adicionado funções para: lista de usuário e retorna usuário pelo login e password

--- DAO/class/Users.php
+++ DAO/class/Users.php
@@ -79,6 +79,56 @@ class Users{
         }
     }
 
+    //retornar lista de usuário
+    public static function index(){
+
+        $sql = new Sql();
+
+       return $sql->select("SELECT * FROM tb_usuarios ORDER BY idusuario");
+
+    }
+
+    public static function search($login){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios where deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=> "%".$login."%"
+        ));
+
+    }
+
+    //retornar usuário pelo login e password
+    public function login($login, $password){
+
+        $sql = new Sql();
+
+        $resultSql = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN  and dessenha = :PASSWORD",
+         array(
+
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+
+        ));
+
+        if(count($resultSql)>0){
+
+            $row = $resultSql[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+        else{
+            throw new Exception("Login ou senha inválido.");
+            
+        }
+
+    }
+
+
+    //retornar um usuário pelo ID
     public function __toString(){
         return json_encode(array(
             "idusuario"=>$this->getIdusuario(),
