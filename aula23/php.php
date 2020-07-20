6189fba957be39e477134acf871d1d8a0fb104af
mateusamarall <mateus.amaral018@gmail.com>
2020-07-20 19:17:13 -0300

    resolvido problemas formato de data

--- aula23/php.php
+++ aula23/php.php
@@ -14,20 +14,24 @@
 //       echo 1; 
 //  }
 
- $data_inicio = new DateTime("19-07-2020");
-$data_fim = new  DateTime("31-12-2004");
-$data_recortada = explode("-","31-12-2004");
+$data = date("2020-07-20");
+ $data_inicio = new DateTime($data);
+$data_fim = new  DateTime("2004-12-31");
+$data_recortada = explode("-",$data);
     
-    $d = $data_recortada[1];
-    $m = $data_recortada[0];
-    $a = $data_recortada[2];
+    $d = $data_recortada[2];
+    $m = $data_recortada[1];
+    $a = $data_recortada[0];
 
+
+    
 
 $dateInterval = $data_inicio->diff($data_fim);
 
 $datatempo = $dateInterval->days;
+echo $datatempo;
 
-$resposta =checkdate($d, $m, $a);
+$resposta =checkdate($m, $d, $a);
 
 if($resposta ===false || $datatempo<5677){
     echo 0;
@@ -36,4 +40,5 @@ if($resposta ===false || $datatempo<5677){
 else{
     echo 1; 
 }
+
 ?>
